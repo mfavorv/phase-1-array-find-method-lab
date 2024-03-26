45cdc66fd15c025bb78fd92ab34e138bb5d3a7f1
Favor Vincent <vfavorm77@gmail.com>
2024-03-26 13:01:51 +0300

    JS array find method lab complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function superbowlWin(games){
+const finder = games.find(game => game.result === "W");
+
+if (finder) {
+    return finder.year;
+} else {
+    return undefined;
+}
+}
+
+const games = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+]
+
+console.log(superbowlWin(games))
+
